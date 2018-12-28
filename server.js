725b77c2f3adce873d3abb84232c3ad4c4645b06
Brian Be <bebrian458@gmail.com>
2018-12-27 19:11:19 -0800

    updated package.json

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require("express");
+var path = require("path");
+var PORT = process.env.PORT || 5000;
 
-var app = express()
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // paths
-app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => res.render('index.html'))
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => res.render("index.html"));
 
 // listen on port
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
